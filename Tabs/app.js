705eb03bbf2c336c83d9c7692f12bd70a3f5fa87
LaxmishNaik <113251527+LaxmishNaik@users.noreply.github.com>
2022-09-13 22:25:18 +0530

    Add files via upload

---
+++ Tabs/app.js
@@ -0,0 +1,20 @@
+const btn = document.querySelectorAll('.tab-btn');
+const about = document.querySelector('.about');
+const article = document.querySelectorAll('.content');
+
+about.addEventListener("click", function(e){
+    const id = e.target.dataset.id;
+    if(id){
+        // remove active from all buttons 
+        btn.forEach(function(btn){
+            btn.classList.remove("active");
+            e.target.classList.add("active");
+        });
+        // hide all the articles
+        article.forEach(function(article){
+            article.classList.remove("active");
+        });
+        const element = document.getElementById(id);
+        element.classList.add("active");
+    }
+});
